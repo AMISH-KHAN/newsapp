9afa536c8c70bed69d655e1eb97edaebcba14ef8
AMISH-KHAN <khanamish8923@gmail.com>
2023-08-23 01:19:44 +0530

    readme added

--- src/Components/Newssitem.jsx
+++ src/Components/Newssitem.jsx
@@ -10,7 +10,7 @@ export default class Newssitem extends Component {
   <div className="card-body"  >
                         <h5 className="card-title" style={{ height: 90 + "px" }} >{this.props.title.slice(0, 60) + "...."}</h5>
                         <p className='source text-center'>{this.props.source.slice(0,20)+"..."}-{`${new Date(this.props.time).getDate()}/${new Date(this.props.time).getMonth()}/${new Date(this.props.time).getFullYear()}` }</p>
-                       
+
                         <hr />
         <p className="card-text"  style={{ height: 150 + "px" }}>{this.props.desc.slice(0,130)+"..."}</p>
                         <a href={this.props.url} className="btn btn-primary btn-sm background w-100 text-center">Read More</a>
